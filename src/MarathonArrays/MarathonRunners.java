0c378f36378f938db640d0bac786640c40aa1f6e
ledvora <ledvora@gmail.com>
2014-08-05 13:26:18 -0700

    adding a new test framework for finding lowest values

---
+++ src/MarathonArrays/MarathonRunners.java
@@ -0,0 +1,27 @@
+package MarathonArrays;
+
+public class MarathonRunners {
+    public static void main(String[] arguments) {
+	String[] names = { "Elana", "Thomas", "Hamilton", "Suzie", "Phil",
+		"Matt", "Alex", "Emma", "John", "James", "Jane", "Emily",
+		"Daniel", "Neda", "Aaron", "Kate" };
+
+	int[] times = { 343, 273, 248, 329, 445, 402, 388, 275, 241, 334, 412,
+		393, 299, 343, 317, 265 };
+
+	int fastestCurrentTime = times[0];
+
+	System.out.println(names.length); // Double checking that the two lists
+	System.out.println(times.length); // have the same length... I hand
+					  // wrote it, what a joy.
+
+	for (int i = 0; i < names.length; i++) {
+	    if (times[i] < fastestCurrentTime) {
+		fastestCurrentTime = times[i];
+
+	    };
+	};
+
+	System.out.println(fastestCurrentTime);
+    }
+}
