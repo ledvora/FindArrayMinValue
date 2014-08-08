92aa8305120b50ddf48ed166ac57d45003bacdf7
ledvora <ledvora@gmail.com>
2014-08-07 19:04:48 -0700

    keeping track of index instead of time and name, a simpler way of doing it.

--- src/MarathonArrays/MarathonRunners.java
+++ src/MarathonArrays/MarathonRunners.java
@@ -6,21 +6,21 @@ public class MarathonRunners {
 		"Matt", "Alex", "Emma", "John", "James", "Jane", "Emily",
 		"Daniel", "Neda", "Aaron", "Kate", "Dean" };
 
-	int[] times = { 343, 273, 248, 329, 445, 402, 388, 300, 241, 334, 412,
-		393, 299, 343, 317, 265, 355 };
+	int[] times = { 343, 273, 248, 329, 445, 402, 388, 300, 24166, 334, 412,
+		393, 299, 343, 317, 265, 3666 };
 
-	int fastestCurrentTime = times[0];
-        String fastestCurrentName = names[0];
-
+	int fastestCurrentIndex = 0;
+        
+        
 	for (int i = 0; i < names.length; i++) {
-	    if (times[i] < fastestCurrentTime) {
-		fastestCurrentTime = times[i];
-                fastestCurrentName = names[i];
+	    if (times[i] < times[fastestCurrentIndex]) {
+		fastestCurrentIndex = i;
 
 	    }
 	}
 
-	System.out.println("The fastest person was: "  + fastestCurrentName + 
-                '\n' + "with a time of: " + fastestCurrentTime);
+	System.out.println("The fastest person was: "  + 
+                names[fastestCurrentIndex] + '\n' + "with a time of: " + 
+                times[fastestCurrentIndex]);
     }
 }
